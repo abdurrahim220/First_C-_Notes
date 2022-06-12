b1dc36eab6d68f292fe20a6b04b99d48a8c2bc78
abdurrahim220 <z.abdurrahim5@gmail.com>
2022-06-12 18:00:33 +0600

    MaxMin

--- findMax_Min.cpp
+++ findMax_Min.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
-    cout<<"helo world";
+    int array[10];
+    int i, max,min,size;
+    cout<<"Enter size of the array= ";
+    cin>>size;
+    cout<<"\n Enter "<<size <<" elements in the array: ";
+    for (i=0; i<size; i++)
+    cin>>array[i];
+
+    max = array[0];
+    min = array[0];
+    
+    for(i=1; i<size; i++)
+    {
+        if(array[i]>max)
+        max = array[i];
+        
+        if(array[i]<min)
+        min = array[i];
+    }
+    cout<<"\nMaximum elemnt = " <<max <<endl;
+    cout<<"\nMinmum element = " <<min <<endl;
+
     return 0;
+
 }
